5559f6265e959ac9d629bc8567e557e6f8c2117d
Rebecca Cole <g00364727@gmit.ie>
2019-11-07 14:52:41 +0000

    added code from Dominics example

--- shop.c
+++ shop.c
@@ -196,10 +196,6 @@ int checkQuantity(struct Shop shop, struct ItemOrder item){
 
 }
 
-int findProduct(struct Shop shop, struct ItemOrder item ){
-
-}
-
 double findProductPrice(struct Shop s, char *n)
 {
 	for(int i = 0; i < s.index; i++)
@@ -246,8 +242,37 @@ int main(void)
 	double price = findProductPrice(shop, "Coke Can");
 	printf("%.2f\n", price);
 
+		// this is the customers budget
+	double budget = 100.0;
+	// this is how much money the shop has
+	double shopCash = 2000.0;
+	// for simplicity we are saying the customer always wants 1 of each item
+	int quantity = 1;
+	
+	// These are two products
+	struct Product a = { "Coke Can", 1.10 };
+	struct Product b = { "Mirror", 20.40 };
+	
+	// create an array to store the products
+	struct Product cp[] = { a, b };
+	
+	double cost = 0;
+	// We will loop over the array to find out how much it will cost the customer to buy all these items
+	for(int i=0; i < 2; i++)
+	{
+		cost += cp[i].price * quantity;
+	} 
+	// in this simple example the customer either can afford it all or nothing, you will want to be able to let them buy what they can afford
+	if (cost <= budget)
+	{
+		// we reduce the customers cash
+		budget -= cost;
+		// the shop gets the money
+		shopCash += cost;
+		printf("\nThe customer can buy everything they wanted and now the shop has %.2f and the customer has %.2f remaining\n", shopCash, budget);
+
 	
 // printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
 	
     return 0;
-}
+}}
